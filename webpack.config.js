cdddd326e03549d4ec59a95bbb5672dfac374bcb
Bruk Belachew <brukb@microsoft.com>
2016-07-12 06:55:48 -0700

    Trying awesome-typescript-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
+var TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
+
 module.exports = {
-  context: __dirname,
-  entry: {
-    library: __dirname + '/src/index'
-  },
-  output: {
-    library: 'mwf',
-    libraryTarget: 'umd',
-    path: __dirname + '/out',
-    filename: 'MWF-main-2.js'
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
-  },
-  module: {
-      loaders: [
-        { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }
-      ]
-    }
+    context: __dirname,
+    entry: {
+        library: __dirname + '/src/index'
+    },
+    output: {
+        library: 'mwf',
+        libraryTarget: 'umd',
+        path: __dirname + '/out',
+        filename: 'MWF-main-2.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+        plugins: [
+            new TsConfigPathsPlugin( './tsconfig.json')
+        ]
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader',
+            exclude: /node_modules/
+        }]
+    },
+
 }
